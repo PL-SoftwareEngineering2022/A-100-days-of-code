26a554123b9ec59466de5bd2198f67850aa57183
phyllis likimani <likiphyllis@yahoo.com>
2023-02-06 20:11:24 -0600

    day1

--- README.md
+++ README.md
@@ -1,33 +1,44 @@
 # A 100 Days of Code
 ### Day 1   - Printing, Commenting, Debugging, String Manipulation and Variables
-                - Printing to the console in Python
+                - Printing to the Console in Python
                 - String Manipulation and Code Intelligence
                 - Debugging
                 - Python Input Function
                 - Python Variables
                 - Variable Naming
-### Day 2   - Data Types, Numbers, Operations, Type Conversion, f-Strings
+### Day 2   - Data Types, Numbers, Operations, Type Conversion, 'f-Strings'
                 - Python Primitive Data Types
                 - Type Error, Type Checking and Type Conversion
                 - Mathematical Operators in Python
-                - Number Manipulation and f-Strings in Python
+                - Number Manipulation and 'f-Strings' in Python
 ### Day 3   - Conditional Statements and Logical Operators
-                - Control Flow with if/else and Conditional Operators
-                - Nested if Statements and elif Statements
-                - Multiple If Statements in Succession
+                - Control Flow with 'if/else' and Conditional Operators
+                - Nested 'if' Statements and 'elif' Statements
+                - Multiple 'if' Statements in Succession
                 - Logical Operators
 ### Day 4   - Randomization and Python Lists
                 - Random Module
                 - Understanding the offset and Appending Items to Lists
                 - Index Errors and Working with Nested Lists
 ### Day 5   - Python Loops
-                - Using the for loop with Python Lists
+                - Using the 'for loop' with Python Lists
                 - for loops and the range() function
 ### Day 6   - Python Functions and Karel
                 - Defining and Calling Python Functions
                 - Indentation in Python
                 - While Loops
-                - Jumping over Hurdles using While Loops
+                - Jumping over Hurdles Using While Loops
 ### Day 7   - Hangman
                 - Break a Complex Problem Down into a Flow Chart
-### 
+### Day 8   - Function Parameters and Caesar Cipher
+                - Functions with Inputs
+                - Positional vs. Keyword Arguments
+                - Caesar Cipher Part 1: Encryption
+                - Caesar Cipher Part 2: Decryption
+                - Caesar Cipher Part 3: Reorganising Our Code
+                - Caesar Cipher Part 4: User Experience Improvements and Final Touches
+### Day 9   - 
+
+### References:
+1. https://www.w3schools.com/python/default.asp
+2. 
--- day1.py
+++ day1.py
@@ -1,4 +1,6 @@
 # printing, commenting, debugging, string manipulation and variables
+# ==================================================================
+
 #DataType: string
 
 # print() echoes back the string or characters in between the ""
@@ -31,7 +33,7 @@ print("Hello Phyllis\nHello phyllis\nhello phyllis")
 
 
 #String concatenation:
-# concatenate with space in between- Options: space after Hello, space before phyllis and an empty " " between the two words:
+# Concatenate with space in between- Options: space after Hello, space before phyllis and an empty " " between the two words:
 print("hello " + "phyllis")
     # --> hello phyllis
 print("hello" + " phyllis")
@@ -40,13 +42,19 @@ print("hello" + " " + "phyllis")
     # --> hello phyllis
 
 
-# Instructions
+# Instructions:
 # When you run your program, it should print the following:
 
         # Day 1 - String Manipulation
         # String Concatenation is done with the "+" sign.
         # e.g. print("Hello " + "world")
         # New lines can be created with a backslash and n.
+
+#Fix the code below 👇
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
 
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
@@ -55,47 +63,177 @@ print("New lines can be created with a backslash and n.")
 
 
 # Input:
-# input() will wait for the user to add the input before proceeding:
+# =======
+
+# input() - It creates a prompt for the user. The input function waits for the user to add the input before proceeding:
 # eg:
 input("what is your name? ")
     # --> phyllis
 
+# example use case:
 print("Hello " + input("what is your name? ") + "!")
     # --> what is your name? phyllis
     # --> Hello phyllis!
 
 
-#len() prints the length of the characters in the string
+#len():
+# ======
+
+# the length function prints the length of the characters in the string
+
+# Instructions:
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+# e.g.
+# https://www.google.com/search?q=how+to+get+the+length+of+a+string+in+python+stack+overflow
+# Warning. Your program should work for different inputs. e.g. any name that you input.
+        # Example Input
+        # Angela
+        # Example Output
+        # 6
+
+#Write your code below this line 👇
 print(len(input("what is your name? ")))
     # --> what is your name? phyllis
     # --> 7
 
 
-#variables()
-# variables have to be one word and in python you can use an underscore, eg. user_name, or you can use a digit but it cannot be the at the beginning 
-# of the variable. you can have "length1" but not "1length"
+# variables():
+# ============
+# ref: https://www.w3schools.com/python/python_variables.asp
 
-# you should use the special words like "print" or "input" as a variables as these are functions
+# Variables are containers for storing data values.
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+    # eg.
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
 
-# any mistype/variation from the original variable will cause python not to pick up the variable later in the code, 
-# eg. "name" needs to remain as exactly that for the rest of the code. Any errors like "name" or "nime"... will result in an error of "variable not previously defined."
+# Casting:
+# If you want to specify the data type of a variable, this can be done with casting.
 
+# Example:
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# String variables can be declared either by using single or double quotes:
+x = "John"
+# is the same as
+x = 'John'
+
+# Variable names are case-sensitive.
+# Example
+# This will create two variables:
+
+a = 4
+A = "Sally"
+    # A will not overwrite a
+
+# Variable Rules:
+# 1. A variable name must start with a letter or the underscore character. variables have to be one word and in python you can use an underscore, eg. user_name, or _user_name.
+# 2. You can use a digit but it cannot be the at the beginning of the variable. you can have "length1" but not "1length"
+# 3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# 4. Variable names are case-sensitive (age, Age and AGE are three different variables)
+# 5. You should use the special words like "print" or "input" as a variables as these are functions
+# 6. Any mistype/variation from the original variable will cause python not to pick up the variable later in the code, 
+    # eg. "name" needs to remain as exactly that for the rest of the code. 
+    # Any errors like "name" or "nime"... will result in an error of "variable not previously defined."
+
+# Example:
+# Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Example:
+# Illegal variable names:
+    # 2myvar = "John"
+    # my-var = "John"
+    # my var = "John"
+
+# Making variable names more readable:
+# =====================================
+# Variable names with more than one word can be difficult to read.
+# There are several techniques you can use to make them more readable:
+
+# 1. Camel Case:
+# Each word, except the first, starts with a capital letter:
+myVariableName = "John"
+
+# 2. Pascal Case:
+# Each word starts with a capital letter:
+MyVariableName = "John"
+
+# 3. Snake Case:
+# Each word is separated by an underscore character:
+my_variable_name = "John"
+
+# Many Values to Multiple Variables:
+# Python allows you to assign values to multiple variables in one line:
+# Example:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+    # Orange
+    # Banana
+    # Cherry
+# Note: Make sure the number of variables matches the number of values, or else you will get an error.
+
+# One Value to Multiple Variables:
+# And you can assign the same value to multiple variables in one line:
+# Example
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+    # Orange
+    # Orange
+    # Orange
+
+# Use case:
 name = input("what is your name? ")
 length= len(name)
 print(length)
     # --> what is your name? phyllis
     # --> 7
 
-# Instructions
+# Instructions:
 #Write a program that switches the values stored in the variables a and b.
-#Your program should work for different inputs. e.g. any value of a and b.
+# Example Input
+    # a: 3
+    # b: 5
+# Example Output
+    # a: 5
+    # b: 3
+
+####Hint:####
+# You should not have to type any numbers in your code.
+# You might need to make some more variables.
+
+# Your program should work for different inputs. e.g. any value of a and b.
+# 🚨 Don't change the code below 👇
 a = input("a: ")
 b = input("b: ")
-#solution: Think about it like switching liquids in two cups, you get a third cup to make this possible
-c=a
-a=b
-b=c
-
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+# solution: Think about it like switching liquids in two cups, you get a third cup to make this possible
+c = a
+a = b
+b = c
+# Write your code above this line 👆
+# 🚨 Don't change the code below 👇
 #test:
 print("a: " + a)
 print("b: " + b)
@@ -103,14 +241,26 @@ print("b: " + b)
     # b: 8
     # a: 8
     # b: 6
+# 🚨 Don't change the code above 👆
 
 
-# band name generator:
-# Make sure the input cursor shows on a new line.
-print("Hello! Welcome to the band generator.")
-city = input("what is the name of the city you grew up in?\n ")
-pet = input("what is the name of your first pet?\n ")
-print("the name of your band could be: " + city + " " + pet )
-    # what is the name of the city you grew up in? Nairobi 
-    # what is the name of your first pet? Chips
-    # the name of your band could be: Nairobi Chips
+#================================
+# Project - Band Name Generator:
+# ===============================
+
+#1. Create a greeting for your program.
+#2. Ask the user for the city that they grew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line
+
+print("Hello! Welcome to the band name generator.")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your first pet?\n")
+print("The name of your band could be: " + city + " " + pet )
+    # Hello! Welcome to the band name generator.
+    # What is the name of the city you grew up in?
+    # Nairobi
+    # What is the name of your first pet?
+    # Chips
+    # The name of your band could be: Nairobi Chips
